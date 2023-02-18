fd65785fb049d970c92fbc44797159106ee5793e
Andrew Kamal <kamalandrew55@gmail.com>
2023-02-17 22:19:31 -0500

    Create mainnet.sol

---
+++ mainnet.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.17;
+contract Mainnet_Vault {
+    /**
+     * @dev will send to vault */    
+    string public RPC_URL = "PLEASE EDIT";
+    string public symbol = "PLEASE EDIT"; 
+    string public ChainID = "PLEASE EDIT";
+    string public EVM_TotalSupply = "PLEASE EDIT";
+}
