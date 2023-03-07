0b4661dcf56ad093fff22ac40e9bb0bbda584274
Andrew Kamal <kamalandrew55@gmail.com>
2023-03-07 15:53:47 -0500

    Update sidenet.sol

--- sidenet.sol
+++ sidenet.sol
@@ -6,4 +6,6 @@ contract Sidenet_Vault {
     string public symbol = "CCS"; 
     string public ChainID = "112";
     string public EVM_TotalSupply = "10000000";
+    string public DeployerSender = "0x0D81d9E21BD7C5bB095535624DcB0759E64B3899";
+
 }
