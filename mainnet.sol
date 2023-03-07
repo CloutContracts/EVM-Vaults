a445fd62cbb0eafcdda6e20013b54bc1bfdccc8a
Andrew Kamal <kamalandrew55@gmail.com>
2023-03-07 15:44:36 -0500

    Update mainnet.sol

--- mainnet.sol
+++ mainnet.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.8.17;
 contract Mainnet_Vault {
     /**
      * @dev will send to vault */    
-    string public RPC_URL = "PLEASE EDIT";
-    string public symbol = "PLEASE EDIT"; 
-    string public ChainID = "PLEASE EDIT";
-    string public EVM_TotalSupply = "PLEASE EDIT";
+    string public RPC_URL = "evm.cloutcontracts.net";
+    string public symbol = "CCS"; 
+    string public ChainID = "12";
+    string public EVM_TotalSupply = "44000000";
 }
