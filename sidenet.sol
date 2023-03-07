1d7750fc8e4edef174c8c0bc7dbb256759e0d3a7
Andrew Kamal <kamalandrew55@gmail.com>
2023-03-07 15:46:52 -0500

    Update sidenet.sol

--- sidenet.sol
+++ sidenet.sol
@@ -4,6 +4,6 @@ contract Sidenet_Vault {
      * @dev will send to vault */    
     string public RPC_URL = "side-evm.cloutcontracts.net";
     string public symbol = "CCS"; 
-    string public ChainID = "112;
+    string public ChainID = "112";
     string public EVM_TotalSupply = "10000000";
 }
