1b3f52b5eb499b75617024881d3cbe5e6a5bb314
Andrew Kamal <kamalandrew55@gmail.com>
2023-03-07 15:43:37 -0500

    Update sidenet.sol

--- sidenet.sol
+++ sidenet.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.8.17;
 contract Sidenet_Vault {
     /**
      * @dev will send to vault */    
-    string public RPC_URL = "PLEASE EDIT";
-    string public symbol = "PLEASE EDIT"; 
-    string public ChainID = "PLEASE EDIT";
-    string public EVM_TotalSupply = "PLEASE EDIT";
+    string public RPC_URL = "side-evm.cloutcontracts.net";
+    string public symbol = "CCS"; 
+    string public ChainID = "112;
+    string public EVM_TotalSupply = "10000000";
 }
